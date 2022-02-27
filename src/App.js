02bba4ef4de8370f4c3230644d12e303fc80e7ad
Leon Kan <leonykan@uw.edu>
2022-02-26 22:53:06 -0800

    bottom navbar semi working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Create from './pages/Create';
+import BottomNavbar from './pages/BottomNavbar';
 
 function App() {
     return (
@@ -8,9 +9,11 @@ function App() {
             <Switch>
                 <Route exact path="/">
                     <Notes />
+                    <BottomNavbar />
                 </Route>
                 <Route path="/create">
                     <Create />
+                    <BottomNavbar />
                 </Route>
             </Switch>
         </Router>
