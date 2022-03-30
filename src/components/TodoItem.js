7f146867d9358a3526b773d99f07d1fd8346a7f1
Leon Kan <leonykan@uw.edu>
2022-03-30 14:30:37 -0700

    list 3 todo option in drawer

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,51 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import { Grid } from '@mui/material';
+import { Drawer, Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Checkbox from '@mui/material/Checkbox';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function TodoItem({ todo, handleStatus }) {
+    const [state, setState] = React.useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState(open);
+    };
+
+    const list = () => (
+        <Box
+            sx={{ width: 'auto' }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {/* TODO: implement 3 actions */}
+                {['Assign', 'Edit', 'Delete'].map((text) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                <ListItem button key="cancel">
+                    <ListItemText primary="Cancel" />
+                </ListItem>
+            </List>
+        </Box>
+    );
+
     return (
         <Card key={todo.key}
             sx={{
@@ -15,7 +54,7 @@ export default function TodoItem({ todo, handleStatus }) {
             }}>
             <Grid container>
 
-                <Grid item md={1}>
+                <Grid item>
                     <CardActions>
                         <Checkbox
                             tabIndex={-1}
@@ -25,7 +64,7 @@ export default function TodoItem({ todo, handleStatus }) {
                     </CardActions>
                 </Grid>
 
-                <Grid item md={1}>
+                <Grid item>
                     <CardContent>
                         <Typography variant="h5" component="div">
                             {todo.name}
@@ -39,13 +78,21 @@ export default function TodoItem({ todo, handleStatus }) {
                     </CardContent>
                 </Grid>
 
-                {/* FIXME: display 3dots in the same row as text above */}
-                {/* TODO: use Drawer to list 3dots options */}
-                <Grid container justifyContent="flex-end">
+                <Grid item>
                     <CardActions>
-                        <MoreVertIcon />
+                        {<React.Fragment key='bottom-drawer-frag'>
+                            <MoreVertIcon onClick={toggleDrawer(true)} />
+                            <Drawer
+                                anchor='bottom-drawer'
+                                open={state}
+                                onClose={toggleDrawer(false)}
+                            >
+                                {list()}
+                            </Drawer>
+                        </React.Fragment>}
                     </CardActions>
                 </Grid>
+
             </Grid>
         </Card>
     );
