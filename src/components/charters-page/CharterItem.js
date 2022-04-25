652c143acc773a29d65709326696717815ae07cd
Leon Kan <leonykan@uw.edu>
2022-04-24 18:00:24 -0700

    emoji react to posts

--- src/components/charters-page/CharterItem.js
+++ src/components/charters-page/CharterItem.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Picker from 'emoji-picker-react';
+import Popper from '@mui/material/Popper';
 
 // title: fontcolor varies when `charters` or `charter templates`
 // content: line breaks, calendar view, placeholder
 
 export default function CharterItem({ item }) {
+    const [chosenEmoji, setChosenEmoji] = React.useState([]);
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(anchorEl ? null : event.currentTarget);
+    };
+
+    const onEmojiClick = (event, emojiObject) => {
+        const list = chosenEmoji;
+        list.push(emojiObject)
+        setChosenEmoji(list);
+        setAnchorEl(null);
+        /** TODO: post API with new emoji list */
+    }
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popper' : undefined;
+
+    console.log(chosenEmoji);
+
     return (
         <div>
             <Box
@@ -22,6 +45,17 @@ export default function CharterItem({ item }) {
             >
                 <h2>{item.title || item.name}</h2>
                 <p>{item.content}</p>
+                <button aria-describedby={id} type="button" onClick={handleClick}>
+                    React
+                </button>
+                <Popper id={id} open={open} anchorEl={anchorEl}>
+                    <Picker onEmojiClick={onEmojiClick} />
+                </Popper>
+                {chosenEmoji.length > 0 ? (
+                    <span>You chose: {chosenEmoji.map(item => item.emoji)}</span>
+                ) : (
+                    <span>No emoji chosen</span>
+                )}
             </Box>
         </div>
     );
