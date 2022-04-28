7f10728c1866934d2644a6159469c0687ee3297b
Leon Kan <57278679+leonkansh@users.noreply.github.com>
2022-04-27 21:20:17 -0700

    prototype teambase with style & add meeting page & archive charters

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Home from './pages/Home';
 import Tasks from './pages/Tasks';
 import Chats from './pages/Chats';
 import Teambase from './pages/Teambase';
-import Charters from './pages/Charters';
+// import Charters from './pages/Charters';
 import Reflections from './pages/Reflections';
 import TodoCreation from './pages/TodoCreation';
 import PostCreation from './pages/PostCreation';
-import CharterTemplate from './pages/CharterTemplate';
-import CharterCreation from './pages/CharterCreation';
+// import CharterTemplate from './pages/CharterTemplate';
+// import CharterCreation from './pages/CharterCreation';
+import Meeting from './components/teambase/Meeting';
 
 function App() {
     return (
@@ -18,10 +19,11 @@ function App() {
                 <Route exact path="/tasks" component={Tasks} />
                 <Route path="/tasks/create-todo" component={TodoCreation} />
                 <Route path="/chats" component={Chats} />
-                <Route path="/teambase" component={Teambase} />
-                <Route exact path="/charters" component={Charters} />
+                <Route exact path="/teambase" component={Teambase} />
+                <Route path="/teambase/meetings" component={Meeting} />
+                {/* <Route exact path="/charters" component={Charters} />
                 <Route path="/charters/charter-templates" component={CharterTemplate} />
-                <Route path="/charters/create-charter" component={CharterCreation} />
+                <Route path="/charters/create-charter" component={CharterCreation} /> */}
                 <Route exact path="/reflections" component={Reflections} />
                 <Route path="/reflections/create-post" component={PostCreation} />
             </Switch>
