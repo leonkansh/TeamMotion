ff25dc9c5588da8ca2a20d33a3d7860fceafc238
Leon Kan <leonykan@uw.edu>
2022-05-07 14:40:17 -0700

    make adjustable headerbar height

--- src/components/nav/HeaderBar.js
+++ src/components/nav/HeaderBar.js
@@ -7,20 +7,20 @@ import './HeaderBar.css';
  * @screenname { team name passed as string in a prop } 
  * @returns Rendering a house icon to go home screen and (TODO)click to edit team name
  */
-export default function HeaderBar({ screenname }) {
+export default function HeaderBar({ screenname, barHeight }) {
     const btnHomeStyle = {
         paddingLeft: "20px",
         position: "absolute"
     }
     
     return (
-        <div className='header-1-container'>
-            <Link className="btn-home" to="/" style={btnHomeStyle}>
-                <House size={35} color="#383E56"/>
-            </Link>
-            <div className='header-title'>
-                <p>{screenname}</p>
-            </div>
+      <div className="header-1-container" style={{ height: `${barHeight}vh` }}>
+        <Link className="btn-home" to="/" style={btnHomeStyle}>
+          <House size={35} color="#383E56" />
+        </Link>
+        <div className="header-title">
+          <p>{screenname}</p>
         </div>
+      </div>
     );
 }
