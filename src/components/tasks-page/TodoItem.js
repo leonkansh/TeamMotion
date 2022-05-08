43f69885ee84b6cd6e7fdeb7cde94f7468c5ca0c
Leon Kan <leonykan@uw.edu>
2022-05-07 17:33:00 -0700

    style todo item

--- src/components/tasks-page/TodoItem.js
+++ src/components/tasks-page/TodoItem.js
@@ -1,52 +1,8 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import { Grid, IconButton } from '@mui/material';
-import Typography from '@mui/material/Typography';
-import Checkbox from '@mui/material/Checkbox';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import React from 'react';
+import { TrashSimple } from "phosphor-react";
+import './TodoItem.css';
 
 export default function TodoItem({ todo, handleStatus, handleDelete }) {
-    const [state, setState] = React.useState(false);
-
-    const toggleDrawer = (open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-
-        setState(open);
-    };
-
-    const list = () => (
-        <Box
-            sx={{ width: 'auto' }}
-            role="presentation"
-            onClick={toggleDrawer(false)}
-            onKeyDown={toggleDrawer(false)}
-        >
-            <List>
-                {/* TODO: implement 3 actions */}
-                {['Assign', 'Edit', 'Delete'].map((text) => (
-                    <ListItem button key={text}>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                <ListItem button key="cancel">
-                    <ListItemText primary="Cancel" />
-                </ListItem>
-            </List>
-        </Box>
-    );
-
     const convertUTC = (input_date) => {
         var options = { weekday: 'long', month: 'short', day: 'numeric' };
         var date = new Date(input_date);
@@ -64,45 +20,29 @@ export default function TodoItem({ todo, handleStatus, handleDelete }) {
     }
 
     return (
-        <Card
-            sx={{
-                minWidth: 275
-            }}>
-            <Grid container>
-
-                <Grid item>
-                    <CardActions>
-                        <Checkbox
-                            tabIndex={-1}
-                            disableRipple
-                            defaultChecked={todo.completed}
-                            onChange={e => { handleStatus(todo) }}
-                        />
-                        {/* TODO: update assignment context */}
-                    </CardActions>
-                </Grid>
-
-                <Grid item>
-                    <CardContent>
-                        <Typography variant="h5" component="div">
-                            {todo.content}
-                        </Typography>
-                        <Typography variant="h7" component="div">
-                            {displayDate(todo.date)}
-                        </Typography>
-                        <Typography variant="h7" component="div">
-                            {displayUser(todo.userid.name)}
-                        </Typography>
-                    </CardContent>
-                </Grid>
-
-                <Grid item sx={{ position: 'absolute', right: 10 }}>
-                    <IconButton color='error' onClick={e => handleDelete(todo)}>
-                        <DeleteIcon />
-                    </IconButton>
-                </Grid>
-
-            </Grid>
-        </Card>
+      <div /*key={todo.id}*/ className="todo-item">
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => {
+            e.preventDefault();
+            handleStatus(todo);
+            console.log("clicked");
+          }}
+        />
+        <div className="todo-content-container">
+          <p className="todo-content">{todo.content}</p>
+          <p className="todo-date">{displayDate(todo.date)}</p>
+        </div>
+        <div className="assignee-container">
+            <p className="todo-username">
+                {displayUser(todo.userid.name)}
+            </p>
+        </div>
+        <button className="btn-delete-todo" onClick={(e) => handleDelete(todo)}>
+            <TrashSimple size={22} color="#ffffff" />
+        </button>
+      </div>
     );
 }
